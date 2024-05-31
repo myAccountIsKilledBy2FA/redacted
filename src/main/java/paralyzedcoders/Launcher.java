cb50036ae592ea926f44926074e0193c40b51d47
Kevin Liu <MagnificentKevin@hotmail.com>
2024-05-31 08:12:38 +0800

    eric

---
+++ src/main/java/paralyzedcoders/Launcher.java
@@ -0,0 +1,28 @@
+package paralyzedcoders.core;
+
+import paralyzedcoders.engine.GameEngine;
+import paralyzedcoders.engine.IGameLogic;
+import paralyzedcoders.engine.Window;
+import paralyzedcoders.game.DummyGame;
+
+
+
+public class Launcher {
+  public static void main(String[] args) {
+    try {
+        boolean vSync = true;
+        IGameLogic gameLogic = new DummyGame();
+        Window.WindowOptions opts = new Window.WindowOptions();
+        opts.cullFace = false;
+        opts.showFps = true;
+        opts.compatibleProfile = true;
+        opts.antialiasing = true;
+        opts.frustumCulling = false;
+        GameEngine gameEng = new GameEngine("GAME", vSync, opts, gameLogic);
+        gameEng.run();
+    } catch (Exception excp) {
+        excp.printStackTrace();
+        System.exit(-1);
+    }
+}
+}
--- src/main/java/paralyzedcoders/core/Model.java
+++ src/main/java/paralyzedcoders/core/Model.java
@@ -1,5 +1,48 @@
 package paralyzedcoders.core;
 
+
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL30.*;
+
 public class Model {
+    private int vaoId;
+    private int vertexVboId;
+    private int indexVboId;
+    private int vertexCount;
+
+    public Model(float[] vertices, int[] indices) {
+        vertexCount = indices.length;
+
+        vaoId = glGenVertexArrays();
+        glBindVertexArray(vaoId);
+
+        vertexVboId = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vertexVboId);
+        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+        indexVboId = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVboId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+
+        // Define the structure of the vertex data
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        glEnableVertexAttribArray(0);
+
+        glBindVertexArray(0);
+    }
+
+    public void render() {
+        glBindVertexArray(vaoId);
+        .glDrawElements(GL_TRIANGLES, vertexCount, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+    }
+
+    public void cleanup() {
+        glDeleteBuffers(vertexVboId);
+        glDeleteBuffers(indexVboId);
+        glDeleteVertexArrays(vaoId);
+    }
 
 }
